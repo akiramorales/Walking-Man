d037e61cee9669c59e2e4b912802b2d065f5befe
Akira Morales <akira.k.morales@gmail.com>
2019-02-26 13:36:23 -0500

    Add files via upload

---
+++ src/WalkingMan.java
@@ -0,0 +1,14 @@
+/**
+ * @author Akira Morales
+ * Period 6
+ * Walking Man
+ */
+import java.awt.geom.Ellipse2D;
+import javax.swing.JComponent;
+
+public class WalkingMan extends JComponent
+{
+	private Ellipse2D man;
+	
+	
+}
