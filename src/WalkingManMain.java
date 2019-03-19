3691acd98c7df7c1c8b6c064e002b55f2edce7d8
AKIRA <AKIRA@AKIRA-PC.fios-router.home>
2019-03-19 19:09:30 -0400

    testing again

--- src/WalkingManMain.java
+++ src/WalkingManMain.java
@@ -17,11 +17,11 @@ public class WalkingManMain extends JFrame implements ActionListener
 {
 	private WalkingMan walkingMan;
 	private Ball ball;
+	private ArrayList<Ball> ballList = new ArrayList<>();
 	public WalkingManMain()
 	{
 		this.setBounds(100, 100, 600, 600);
 		this.setLayout(null);
-		ArrayList<Ball> ballList = new ArrayList<>();
 		walkingMan = new WalkingMan(120, 120);
 		ball = new Ball(walkingMan.getX()+ 31, walkingMan.getY()+10);
 		this.add(walkingMan);
@@ -54,6 +54,7 @@ public class WalkingManMain extends JFrame implements ActionListener
 				}
 				else if(e.getKeyCode() == KeyEvent.VK_SPACE)
 				{
+					
 					ballList.add(ball);
 					ball.move();
 				}
@@ -80,6 +81,7 @@ public class WalkingManMain extends JFrame implements ActionListener
 				}
 				else if (e.getKeyCode() == KeyEvent.VK_SPACE)
 				{
+					ballList.add(ball);
 					ball.move();
 				}
 			}
@@ -101,9 +103,15 @@ public class WalkingManMain extends JFrame implements ActionListener
 	{
 		if(walkingMan.getX()<0) walkingMan.setLocation(0, walkingMan.getY());
 		if(walkingMan.getY()<0) walkingMan.setLocation(walkingMan.getX(), 0);
-		if(walkingMan.getY()+51 > getHeight()) walkingMan.setLocation(walkingMan.getX(), getHeight());
-		if(walkingMan.getX()+31 > getWidth()) walkingMan.setLocation(getWidth(), walkingMan.getY());
+		if(walkingMan.getY()+51 > getHeight()) walkingMan.setLocation(walkingMan.getX(), getHeight()-51);
+		if(walkingMan.getX()+31 > getWidth()) walkingMan.setLocation(getWidth()-31, walkingMan.getY());
+		if(ball.getX() > getWidth())
+		{
+			ballList.remove(ball);
+			this.remove(ball);
+		}
 		walkingMan.update();
+		ball.update();
 		repaint();
 	}
 	
